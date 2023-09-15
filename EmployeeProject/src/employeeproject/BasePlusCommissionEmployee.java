eac9481ea1e129f670e0cdee72e41526bf5364b2
Plebbaroni <95750980+Plebbaroni@users.noreply.github.com>
2023-09-15 08:23:57 +0800

    Add files via upload

--- EmployeeProject/src/employeeproject/BasePlusCommissionEmployee.java
+++ EmployeeProject/src/employeeproject/BasePlusCommissionEmployee.java
@@ -5,12 +5,112 @@
  */
 package employeeproject;
 
+import java.util.Date;
+
 
 /**
  *
  * @author User
  */
 public class BasePlusCommissionEmployee {
+    private int empID;
+    private String empName;
+    private Date empDateHired;
+    private Date empBirthDate;
+    private double totalSales;
+    private double baseSalary;
     
+    public BasePlusCommissionEmployee(){
+        empID = 0;
+        empName = "xxx";
+        empDateHired = null;
+        empBirthDate = null;
+        totalSales = 0;
+        baseSalary = 0;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName, Date empDateHired, Date empBirthDate, double totalSales, double baseSalary) {
+        this.empID = empID;
+        this.empName = empName;
+        this.empDateHired = empDateHired;
+        this.empBirthDate = empBirthDate;
+        this.totalSales = totalSales;
+        this.baseSalary = baseSalary;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName, double totalSales, double baseSalary) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalSales = totalSales;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Date getEmpDateHired() {
+        return empDateHired;
+    }
+
+    public void setEmpDateHired(Date empDateHired) {
+        this.empDateHired = empDateHired;
+    }
+
+    public Date getEmpBirthDate() {
+        return empBirthDate;
+    }
+
+    public void setEmpBirthDate(Date empBirthDate) {
+        this.empBirthDate = empBirthDate;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public double computeSalary(double totalSales, double baseSalary){
+        double salary = 0;
+        
+        if(totalSales < 50000){
+            salary = totalSales*0.05;
+        }
+        if(totalSales >= 50000 && totalSales < 100000){
+            salary = totalSales*0.2;
+        }
+        if(totalSales >=100000 && totalSales < 500000){
+            salary = totalSales*0.3;
+        }
+        if(totalSales > 500000){
+            salary = totalSales*0.5;
+        }
+        salary += baseSalary;
+        return salary;
+    }
+    
+    public void displayInfo(){
+        System.out.println(String.format("%d, %s, %tD %n, %tD %n, %f, ₱%f, ₱%f", empID, empName, empDateHired, empBirthDate, totalSales, baseSalary, computeSalary(totalSales, baseSalary)));
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%d, %s, %tD %n, %tD %n, %f, ₱%f, ₱%f", empID, empName, empDateHired, empBirthDate, baseSalary, totalSales, computeSalary(totalSales, baseSalary));
+    }
  
 }
